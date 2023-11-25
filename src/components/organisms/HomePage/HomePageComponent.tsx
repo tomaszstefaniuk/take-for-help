259ded5ed68ea4a6e16e7ba99f8c15206332a50c
Tomasz Stefaniuk <41304693+esemku@users.noreply.github.com>
2023-11-25 12:31:43 +0100

    feat: add clean home page

---
+++ src/components/organisms/HomePage/HomePageComponent.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const HomePageComponent: FC = () => {
+  return <div>Home Page</div>;
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+import { HomePage } from "@/components/organisms";
+
+export default HomePage;
