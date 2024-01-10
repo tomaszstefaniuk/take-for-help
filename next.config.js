72aadcb0b8e2cc2bd9f052f24804803deff71591
Tomasz Stefaniuk <41304693+esemku@users.noreply.github.com>
2024-01-10 14:41:04 +0100

    add rewrites in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    if (process.env.NODE_ENV === "production") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: `${process.env.NEXT_PUBLIC_REACT_APP_SERVER_API}/:path*`,
+        },
+      ];
+    } else {
+      return [];
+    }
+  },
+};
 
 module.exports = nextConfig;
